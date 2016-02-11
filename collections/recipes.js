9696d5ae247eb094afeba2ff7d34313c69ed278b
luisbarajasp <luisbarajasp@gmail.com>
2016-02-11 15:15:45 -0600

    Add: Ingredients to recipe
    Add to the recipe an array of ingredients ([Ingredients]) in collections/recipes.js also add inMenu: autovalue false to the Schema

--- collections/recipes.js
+++ collections/recipes.js
@@ -1,10 +1,19 @@
-Recipes = new Meteor.Collection('recipes');
+Recipes = new Mongo.Collection('recipes');
 
 Recipes.allow({
     insert: function(userId,doc) {
         return !!userId;
     }
-})
+});
+
+Ingredient = new SimpleSchema({
+    name: {
+        type: String
+    },
+    amount: {
+        type: String
+    }
+});
 
 RecipeSchema = new SimpleSchema({
     name: {
@@ -14,6 +23,17 @@ RecipeSchema = new SimpleSchema({
     desc: {
         type: String,
         label: "Description"
+    },
+    ingredients: {
+        type: [Ingredient]
+    },
+    inMenu: {
+        type: Boolean,
+        defaultValue: false,
+        optional: true,
+        autoform: {
+            type: "hidden"
+        }
     },
     author: {
         type: String,
