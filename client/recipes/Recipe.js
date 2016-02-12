b5c12b60f6ae1812a7748d27647fd98efea405d8
luisbarajasp <luisbarajasp@gmail.com>
2016-02-11 21:01:38 -0600

    Add: Menu page layout

---
+++ client/recipes/Recipe.js
@@ -0,0 +1,5 @@
+Template.Recipe.events({
+    'click .toggle-menu': function(){
+        Meteor.call('toggleMenuItem',this._id,this.inMenu);
+    }
+});
--- collections/recipes.js
+++ collections/recipes.js
@@ -3,6 +3,9 @@ Recipes = new Mongo.Collection('recipes');
 Recipes.allow({
     insert: function(userId,doc) {
         return !!userId;
+    },
+    update: function(userId,doc) {
+        return !!userId;
     }
 });
 
@@ -54,6 +57,16 @@ RecipeSchema = new SimpleSchema({
         autoform: {
             type: "hidden"
         }
+    }
+});
+
+Meteor.methods({
+    toggleMenuItem: function(id, currentState){
+        Recipes.update(id, {
+            $set: {
+                inMenu: !currentState
+            }
+        });
     }
 });
 
